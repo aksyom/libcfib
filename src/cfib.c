f4127fdcc31cb07700c295e7ad35c7f89ed0fca0
Arto Pekkanen <aksyom@gmail.com>
2017-12-09 00:48:19 +0200

    Overhaul of cfib.c. Now the functions use C11 thread local primitives if supported; the primitives are not only used by inline definitions in the header. Benefits performance regardless if client code uses cfib.h or cfib_C11.h. Instead of using C11 support as library build variant, now the only library build variant is whether the library is profiled build or not. In order to use the library profilers, client code must explicityle link with -lcfib_profiled or -lcfib_profiled_static. Merged all tests into a single test_cfib.c -file. The test program now takes command line argument as to what test it should run. Also, in src/cfib-sysv-amd64.asm made the code more clear that we are calculating an absolute value from rip-relative operand (ie. added rel in brackets).

--- src/cfib.c
+++ src/cfib.c
@@ -1,8 +1,4 @@
-#if _WITH_C11_THREAD_LOCAL
-#include "cfib_C11.h"
-#else
 #include "cfib.h"
-#endif
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -33,30 +29,53 @@
 // @internal Implemented in assembler module
 void _cfib_init_stack(unsigned char** sp, void* start_addr, void* args);
 
-static inline uint_fast32_t _get_sys_page_size() {
-#ifdef _WITH_SYSAPI_POSIX
+static inline uint_fast32_t _get_sys_page_size()
+{
     static uint_fast32_t cached_size = 0;
+#ifdef _WITH_SYSAPI_POSIX
     if(cached_size == 0)
         cached_size = (uint_fast32_t)sysconf(_SC_PAGE_SIZE);
-    return cached_size;
 #elif defined(_WITH_SYSAPI_WINDOWS)
     #error "TODO: WINAPI support."
 #endif
+    return cached_size;
 }
 
-static inline uint_fast32_t _align_size_to_page(uint_fast32_t size) {
+static inline uint_fast32_t _align_size_to_page(uint_fast32_t size)
+{
     uint_fast32_t size_mod = size % _get_sys_page_size();
     if(size_mod != 0)
         size = size - size_mod + _get_sys_page_size();
     return size;
 }
 
+#ifdef _PROFILED_BUILD
 static void*  _prof_map = NULL;
 static size_t _prof_nfaults = 0;
 
 #ifdef _WITH_C11_ATOMICS
 static atomic_flag _prof_map_lock = ATOMIC_FLAG_INIT;
-#endif
+
+#define _atomic_test_and_set(flag) atomic_flag_test_and_set_explicit(flag, memory_order_relaxed)
+#define _atomic_clear(flag) atomic_flag_clear_explicit(flag, memory_order_release)
+#elif defined (_WITH_SYSAPI_WINDOWS) /* #ifdef _WITH_C11_ATOMICS */
+typedef atomic_flag LONG;
+
+static LONG _prof_map_lock = 0;
+
+#define _atomic_test_and_set(flag) InterlockedExchange(flag, 1)
+#define _atomic_clear(flag) InterlockedExchange(flag, 0)
+#else /* #ifdef _WITH_C11_ATOMICS */
+void* _prof_map_lock = NULL;
+
+inline static int _atomic_test_and_set(void *flag) {
+    assert("_atomic_test_and_set() unimplemented!" && 0);
+}
+
+inline static int _atomic_clear(void *flag) {
+    assert("_atomic_clear() unimplemented!" && 0);
+}
+#endif /* #ifdef _WITH_C11_ATOMICS */
 
 #ifdef _WITH_SYSAPI_POSIX
 static pthread_once_t _prof_init_once = PTHREAD_ONCE_INIT;
@@ -130,11 +149,11 @@ static void _prof_init() {
     assert( sigaction(SIGSEGV, &act, &_prof_oact) == 0 );
     fprintf(stderr, "libcfib: Fiber stack profiler enabled.\n");
 }
-#elif defined(_WITH_SYSAPI_WINDOWS)
+#elif defined(_WITH_SYSAPI_WINDOWS) /* #ifdef _WITH_SYSAPI_POSIX */
     #error "TODO: WINAPI support."
-#else
-    #error "Unsupported platform."
-#endif
+#endif /* #ifdef _WITH_SYSAPI_POSIX  */
+
+#endif /* #ifdef _PROFILED_BUILD */
 
 #ifdef _WITH_C11_THREAD_LOCAL
 _Thread_local cfib_t* _cfib_current = NULL;
@@ -148,18 +167,19 @@ static void _cfib_init_tls() {
     assert("libcfib: pthread_key_create() failed to init TLS key !!!" && ret == 0);
 }
 #elif defined (_WITH_SYSAPI_WINDOWS)
-    #error "TODO: WINAPI support."
+    #error "TODO: WINAPI TSL support."
 #endif
 
 cfib_t* cfib_init_thread()
 {
+    cfib_t* ret = NULL;
 #if _WITH_C11_THREAD_LOCAL
     if(_cfib_current != NULL) {
         fprintf(stderr, "libcfib_C11: WARNING: cfib_init_thread() called more than once per thread!\n");
         return _cfib_current;
     }
     _cfib_current = calloc(1, sizeof(cfib_t));
-    return _cfib_current;
+    ret = _cfib_current;
 #elif defined(_WITH_SYSAPI_POSIX)
     pthread_once(&_cfib_init_tls_once, _cfib_init_tls);
     cfib_t** tls = (cfib_t**)pthread_getspecific(_cfib_tls_key);
@@ -171,30 +191,56 @@ cfib_t* cfib_init_thread()
     assert("libcfib: cfib_init_thread() ran out of memory for TLS object !!!" && tls != NULL);
     *tls = calloc(1, sizeof(cfib_t));
     assert("libcfib: cfib_init_thread() ran out of memory for TLS object !!!" && *tls != NULL);
-    int ret = pthread_setspecific(_cfib_tls_key, tls);
-    assert("libcfib: cfib_init_thread() ran out of memory for TLS object !!!" && ret == 0);
-    return *tls;
-#elif defined(_WITH_SYSAPI_WINDOWS)
-    #error "TODO: WINAPI support."
+    int res = pthread_setspecific(_cfib_tls_key, tls);
+    assert("libcfib: cfib_init_thread() ran out of memory for TLS object !!!" && res == 0);
+    ret = *tls;
+#elif defined (_WITH_SYSAPI_WINDOWS)
+    #error "TODO: WINAPI TLS support."
 #endif
-}
-
-cfib_t* cfib_init_thread__prof__()
-{
-    cfib_t* ret = cfib_init_thread();
-#ifdef _WITH_C11_ATOMICS
+#if defined(_PROFILED_BUILD) && defined (_WITH_SYSAPI_POSIX)
     pthread_once(&_prof_init_once, _prof_init);
-#else
-    fprintf(stderr, "libcfib: WARNING: profiling is disabled!\n");
+#elif defined(_PROFILED_BUILD) && defined (_WITH_SYSAPI_WINDOWS)
+    #error "TODO: WINAPI profiling support."
 #endif
     return ret;
 }
 
 cfib_t* cfib_new(void* start_routine, void* args, uint_fast32_t ssize)
 {
-    cfib_t* context = (cfib_t*)calloc(1, sizeof(cfib_t));
-    if(context == NULL)
+    cfib_t* ret = (cfib_t*)calloc(1, sizeof(cfib_t));
+    if(ret == NULL)
         return NULL;
+#ifdef _PROFILED_BUILD
+
+    void *stack_ceiling = NULL;
+    void* vres = NULL;
+    int res = posix_memalign(&stack_ceiling, 1<<20, 1<<20);
+    if(res != 0) {
+        fprintf(stderr, "libcfib: cfib_init__prof__() failed to allocate profiled stack !!!");
+        goto _errexit;
+    }
+    res = mprotect(stack_ceiling, (1<<20) - _get_sys_page_size(), PROT_NONE);
+    assert("libcfib: cfib_init__prof__() failed to apply guard on profiled stack !!!" && res == 0);
+    ret->stack_ceiling = (unsigned char*)stack_ceiling;
+    ret->sp = ret->stack_floor = ret->stack_ceiling + (1<<20);
+    _cfib_init_stack(&ret->sp, start_routine, args);
+    while( _atomic_test_and_set(&_prof_map_lock) )
+        ; /* Spin until lock is acquired */
+    uintptr_t i0 = ((uintptr_t)stack_ceiling>>12) & 0xffff;
+    uintptr_t i1 = ((uintptr_t)stack_ceiling>>(12 + 16)) & 0xffff;
+    void** map0 = (void**)_prof_map;
+    if( map0[i0] == NULL ) {
+        vres = map0[i0] = mmap(0, 1<<16, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+        assert("libcfib: cfib_init__prof__() failed to assert profiler stack map !!!" && vres != MAP_FAILED);
+    }
+    void** map1 = (void**)(map0[i0]);
+    map1[i1] = stack_ceiling;
+    _atomic_clear(&_prof_map_lock);
+    fprintf(stderr, "__prof__: i0 = %lu, i1 = %lu, map0[%lu] = %p, map1[%lu] = %p\n", i0, i1, i0, map0[i0], i1, map1[i1]);
+    fprintf(stderr, "libcfib: New profiled stack @ [%p - %p]\n", (void*)ret->stack_ceiling, (void*)ret->stack_floor);
+
+#else /* #ifdef _PROFILED_BUILD  */
+
 #ifdef _WITH_SYSAPI_POSIX
     if(ssize != 0)
         ssize = _align_size_to_page(ssize);
@@ -219,67 +265,22 @@ cfib_t* cfib_new(void* start_routine, void* args, uint_fast32_t ssize)
         goto _errexit;
     }
 #ifndef __FreeBSD__
-#if defined(__linux__)
-    void *gp = mmap(m, page_size, PROT_NONE, mmap_flags|MAP_FIXED|MAP_NORESERVE, -1, 0);
-#else
-    void *gp = mmap(m, page_size, PROT_NONE, mmap_flags|MAP_FIXED, -1, 0);
-#endif
-    if(gp == MAP_FAILED)
-        fprintf(stderr, "libcfib: WARNING: cfib_new() failed to mmap() guard page for stack!\n");
+    assert("Failed to set guard page!" && mprotect(m, page_size, PROT_NONE) == 0);
     m += page_size;
-#endif /* #ifndef __FreeBSD__  */
-    context->stack_ceiling = (unsigned char*)m;
-    context->sp = context->stack_floor = context->stack_ceiling + ssize;
-    _cfib_init_stack(&context->sp, start_routine, args);
-    return context;
+#endif
+    ret->stack_ceiling = (unsigned char*)m;
+    ret->sp = ret->stack_floor = ret->stack_ceiling + ssize;
+    _cfib_init_stack(&ret->sp, start_routine, args);
 #elif defined (_WITH_SYSAPI_WINDOWS)
     #error "TODO: WINAPI support."
 #endif
-_errexit:
-    free(context);
-    return NULL;
-}
 
-cfib_t* cfib_new__prof__(void* start_routine, void* args, uint_fast32_t ssize)
-{
-#ifdef _WITH_C11_ATOMICS
-    cfib_t* context = (cfib_t*)calloc(1, sizeof(cfib_t));
-    if(context == NULL)
-        return NULL;
-    void *stack_ceiling = NULL;
-    void* vres = NULL;
-    int res = posix_memalign(&stack_ceiling, 1<<20, 1<<20);
-    if(res != 0) {
-        fprintf(stderr, "libcfib: cfib_init__prof__() failed to allocate profiled stack !!!");
-        goto _errexit;
-    }
-    res = mprotect(stack_ceiling, (1<<20) - _get_sys_page_size(), PROT_NONE);
-    assert("libcfib: cfib_init__prof__() failed to apply guard on profiled stack !!!" && res == 0);
-    context->stack_ceiling = (unsigned char*)stack_ceiling;
-    context->sp = context->stack_floor = context->stack_ceiling + (1<<20);
-    _cfib_init_stack(&context->sp, start_routine, args);
-    while(atomic_flag_test_and_set_explicit(&_prof_map_lock, memory_order_relaxed))
-        ; /* Spin until lock is acquired */
-    uintptr_t i0 = ((uintptr_t)stack_ceiling>>12) & 0xffff;
-    uintptr_t i1 = ((uintptr_t)stack_ceiling>>(12 + 16)) & 0xffff;
-    void** map0 = (void**)_prof_map;
-    if( map0[i0] == NULL ) {
-        vres = map0[i0] = mmap(0, 1<<16, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-        assert("libcfib: cfib_init__prof__() failed to assert profiler stack map !!!" && vres != MAP_FAILED);
-    }
-    void** map1 = (void**)(map0[i0]);
-    map1[i1] = stack_ceiling;
-    atomic_flag_clear_explicit(&_prof_map_lock, memory_order_release);
-    fprintf(stderr, "__prof__: i0 = %lu, i1 = %lu, map0[%lu] = %p, map1[%lu] = %p\n", i0, i1, i0, map0[i0], i1, map1[i1]);
-    fprintf(stderr, "libcfib: New profiled stack @ [%p - %p]\n", (void*)context->stack_ceiling, (void*)context->stack_floor);
-    return context;
+#endif /* #ifdef _PROFILED_BUILD */
+_exit:
+    return ret;
 _errexit:
-    free(context);
+    free(ret);
     return NULL;
-#else
-    fprintf(stderr, "libcfib: WARNING: profiling is disabled!\n");
-    return cfib_new(start_routine, args, ssize);
-#endif
 }
 
 void cfib_unmap(cfib_t* context) {
@@ -305,10 +306,12 @@ void _cfib_exit_thread() {
 #endif
 }
 
-#ifndef _WITH_C11_THREAD_LOCAL
 
 cfib_t* cfib_get_current() {
-#ifdef _WITH_SYSAPI_POSIX
+#ifdef _WITH_C11_THREAD_LOCAL
+    assert("CALL cfib_init_thread() BEFORE CALLING cfib_get_current() !!!" && _cfib_current != NULL);
+    return _cfib_current;
+#elif defined (_WITH_SYSAPI_POSIX)
     cfib_t** tls = (cfib_t**)pthread_getspecific(_cfib_tls_key);
     assert("CALL cfib_init_thread() BEFORE CALLING cfib_get_current() !!!" && tls != NULL);
     return *tls;
@@ -318,8 +321,9 @@ cfib_t* cfib_get_current() {
 }
 
 cfib_t* cfib_get_current__noassert__() {
-#ifdef _WITH_SYSAPI_POSIX
-    //fprintf(stderr, "cfib_get_current__noassert__()\n");
+#ifdef _WITH_C11_THREAD_LOCAL
+    return _cfib_current;
+#elif defined (_WITH_SYSAPI_POSIX)
     return *((cfib_t**)pthread_getspecific(_cfib_tls_key));
 #elif defined (_WITH_SYSAPI_WINDOWS)
     #error "TODO: WINAPI support."
@@ -329,7 +333,12 @@ cfib_t* cfib_get_current__noassert__() {
 void _cfib_swap(unsigned char** sp1, unsigned char* sp2);
 
 void cfib_swap(cfib_t *to) {
-#ifdef _WITH_SYSAPI_POSIX
+#ifdef _WITH_C11_THREAD_LOCAL
+    assert("CALL cfib_init_thread() BEFORE CALLING cfib_swap() !!!" && _cfib_current != NULL);
+    cfib_t* from = _cfib_current;
+    _cfib_current = to;
+    _cfib_swap(&from->sp, to->sp);
+#elif defined (_WITH_SYSAPI_POSIX)
     cfib_t** tls = (cfib_t**)pthread_getspecific(_cfib_tls_key);
     assert("CALL cfib_init_thread() BEFORE CALLING cfib_swap() !!!" && tls != NULL);
     cfib_t* from = *tls;
@@ -341,8 +350,11 @@ void cfib_swap(cfib_t *to) {
 }
 
 void cfib_swap__noassert__(cfib_t *to) {
-#ifdef _WITH_SYSAPI_POSIX
-    //fprintf(stderr, "cfib_swap__noassert__()\n");
+#ifdef _WITH_C11_THREAD_LOCAL
+    cfib_t* from = _cfib_current;
+    _cfib_current = to;
+    _cfib_swap(&from->sp, to->sp);
+#elif defined (_WITH_SYSAPI_POSIX)
     cfib_t** tls = (cfib_t**)pthread_getspecific(_cfib_tls_key);
     cfib_t* from = *tls;
     *tls = to;
@@ -352,4 +364,3 @@ void cfib_swap__noassert__(cfib_t *to) {
 #endif
 }
 
-#endif /* #ifndef _WITH_C11_THREAD_LOCAL  */
--- src/test_cfib.c
+++ src/test_cfib.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-#if _WITH_C11_THREAD_LOCAL
+#include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <sys/mman.h>
+
+#if _TEST_C11_BUILD
 #include "cfib_C11.h"
 #else
 #include "cfib.h"
@@ -7,24 +13,127 @@
 
 cfib_t *main_context;
 
-void test_fiber(void *arg) {
-    printf("test_fiber 1\n");
-    cfib_swap__noassert__(main_context);
-    printf("test_fiber 2\n");
+#define NUM_SAMPLES 10000
+
+cfib_t *fib_main;
+
+void test_pingpong(void *arg) {
+    while(1)
+        cfib_swap((cfib_t*)arg);
+}
+
+void test_pingpong__noassert__(void *arg) {
+    while(1)
+        cfib_swap__noassert__((cfib_t*)arg);
+}
+
+void test_return(void *arg) {
+    return;
+}
+
+int _long_cmp(const void* a, const void *b) {
+    return (int)( *((long*)a) - *((long*)b) );
+}
+
+long _get_median(long* b, size_t s) {
+    long *m = b + s / 2;
+    if(s % 2 == 0)
+        return (*m + *(m - 1)) / 2;
+    else
+        return *m;
+}
+
+/*void bench_ptrcall(long *intervals, int n) {
+    struct timespec tp0, tp1;
+    void (*func)(void*);
+    func = test_return;
+    for(int i = 0; i < n; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &tp0);
+        func(NULL);
+        clock_gettime(CLOCK_MONOTONIC, &tp1);
+        intervals[i] = tp1.tv_nsec - tp0.tv_nsec;
+    }
+    qsort(intervals, n, sizeof(long), _long_cmp);
+    printf("median\t%ld ns\n", _get_median(intervals, n));
+    printf("   min\t%ld ns\n", intervals[0]);
+    printf("   max\t%ld ns\n", intervals[n - 1]);
+}*/
+
+void bench_swap(int n) {
+    struct timespec tp0, tp1;
+    cfib_t* test_context = cfib_new((void*)test_pingpong, (void*)fib_main, 0);
+    long *intervals = mmap(0, sizeof(long) * n, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp0);
+    long tt = tp0.tv_nsec;
+    for(int i = 0; i < n; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &tp0);
+        cfib_swap(test_context);
+        clock_gettime(CLOCK_MONOTONIC, &tp1);
+        intervals[i] = tp1.tv_nsec - tp0.tv_nsec;
+    }
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp0);
+    tt = tp0.tv_nsec - tt;
+    qsort(intervals, n, sizeof(long), _long_cmp);
+    printf("median\t%ld ns\n", _get_median(intervals, n));
+    printf("   min\t%ld ns\n", intervals[0]);
+    printf("   max\t%ld ns\n", intervals[n - 1]);
+    printf(" total\t%ld ns\n", tt);
+    munmap(intervals, sizeof(long) * n);
+}
+
+void bench_swap__noassert__(int n) {
+    struct timespec tp0, tp1;
+    cfib_t* test_context = cfib_new((void*)test_pingpong__noassert__, (void*)fib_main, 0);
+    long *intervals = mmap(0, sizeof(long) * n, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp0);
+    long tt = tp0.tv_nsec;
+    for(int i = 0; i < n; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &tp0);
+        cfib_swap__noassert__(test_context);
+        clock_gettime(CLOCK_MONOTONIC, &tp1);
+        intervals[i] = tp1.tv_nsec - tp0.tv_nsec;
+    }
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp0);
+    tt = tp0.tv_nsec - tt;
+    qsort(intervals, n, sizeof(long), _long_cmp);
+    printf("median\t%ld ns\n", _get_median(intervals, n));
+    printf("   min\t%ld ns\n", intervals[0]);
+    printf("   max\t%ld ns\n", intervals[n - 1]);
+    printf(" total\t%ld ns\n", tt);
+    munmap(intervals, sizeof(long) * n);
+}
+
+void print_usage(char *name) {
+    fprintf(stderr, "Usage: %s <#>\n\n", name);
+    fprintf(stderr, "#\tTest\n");
+    fprintf(stderr, "1\tbenchmark\n");
+    fprintf(stderr, "2\tbenchmark (noassert)\n");
+    fprintf(stderr, "3\tstack hog (NOT IMPLEMENTED)\n");
 }
 
 int main(int argc, char** argv) {
-    // NOTE: Before any calls to cfib_* functions can be made, 
-    // cfib_init_thread() MUST be called at least ONCE in some thread!
-    // 
-    // If this advice is NOT heeded, libcfib will crash the program on
-    // an assertion with a condoning message.
-    main_context = cfib_init_thread();
-    printf("current after init: %p\n", cfib_get_current());
-    cfib_t* test_context = cfib_new((void*)test_fiber, NULL, 0);
-    printf("main 1\n");
-    cfib_swap__noassert__(test_context);
-    printf("main 2\n");
-    cfib_swap__noassert__(test_context);
+    if(argc < 2)
+        goto errexit;
+    unsigned long test_num = strtoul(argv[1], NULL, 10);
+    fib_main = cfib_init_thread();
+    cfib_t* fib = NULL;
+    long *intervals = NULL;
+    switch(test_num) {
+        case 1:
+            bench_swap(NUM_SAMPLES);
+            break;
+        case 2:
+            bench_swap__noassert__(NUM_SAMPLES);
+            break;
+        case 3:
+            fprintf(stderr, "TODO\n");
+            break;
+        default:
+            goto errexit;
+    }
+exit:
     return 0;
+errexit:
+    print_usage(argv[0]);
+    return 1;
 }
